b6ccebfc3c5968557653dea323ca2109f8ee2c9b
runberg <111742594+runberg@users.noreply.github.com>
2024-06-21 11:50:58 +0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# -----------------------------------
+# UPDATE THE SERVER
+# -----------------------------------
+
+apt update
+apt upgrade
+apt full-upgrade
+apt autoremove
+
+# -----------------------------------
+# INSTALL DOCKER & DOCKER COMPOSE
+# -----------------------------------
+
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+apt install ca-certificates curl -y
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
+
+# -----------------------------------
+# GET COMPOSE FILE AND START
+# -----------------------------------
+
+wget https://github.com/runberg/easy-wg-vps/blob/fc7b99d46981edc6d9f8193ea6c6124591560c92/docker-compose.yml
+docker network create traefik_network
+docker compose up -d
