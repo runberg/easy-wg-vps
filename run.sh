9b0fdc07edc764e044cd144b03c54dfa19a083c4
runberg <111742594+runberg@users.noreply.github.com>
2024-06-21 14:19:33 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,14 +4,20 @@
 # UPDATE THE SERVER
 # -----------------------------------
 
+echo "Updating the server..."
+
 apt update
 apt upgrade
 apt full-upgrade
 apt autoremove
 
+echo "Server updated."
+
 # -----------------------------------
 # INSTALL DOCKER & DOCKER COMPOSE
 # -----------------------------------
+
+echo "Installing docker..."
 
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 apt install ca-certificates curl -y
@@ -22,9 +28,28 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
 
 # -----------------------------------
-# GET COMPOSE FILE AND START
+# GET COMPOSE FILE AND UPDATING
 # -----------------------------------
 
+echo "Getting the docker compose file..."
+
+read -p "Enter the domain name for your server: " domain
+read -p "Enter the email address for LetsEncrypt: " email
+
+echo -n "Enter the password for the WG Admin page: "
+read -s pw
+
 wget https://raw.githubusercontent.com/runberg/easy-wg-vps/main/docker-compose.yml
+
+sed -i -e 's/<DOMAIN>/$domain/g' /docker-compose.yml
+sed -i -e 's/<PASSWORD>/$pw/g' /docker-compose.yml
+sed -i -e 's/<EMAIL>/$email/g' /docker-compose.yml
+
+# -----------------------------------
+# STARTING CONTAINER
+# -----------------------------------
+
+echo "Starting containers..."
+
 docker network create traefik_network
 docker compose up -d
