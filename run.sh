fe28e86803f239ef6d5a3c26f879d554bb28cfe2
runberg <111742594+runberg@users.noreply.github.com>
2024-06-25 10:46:14 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,12 +20,7 @@ echo "Server updated."
 echo "Installing docker..."
 
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-apt install ca-certificates curl -y
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
+apt install docker.io docker-*-plugin
 
 # -----------------------------------
 # GET COMPOSE FILE AND UPDATING
