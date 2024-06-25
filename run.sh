788a6a2b2202bfdeae9c9f30cc08a6a9d113b2f6
runberg <111742594+runberg@users.noreply.github.com>
2024-06-25 12:46:11 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -28,19 +28,25 @@ apt-get install docker.io docker-*-plugin -y
 
 echo "Getting the docker compose file..."
 
+wget "https://raw.githubusercontent.com/runberg/easy-wg-vps/main/docker-compose.yml"
+
 read -p "Enter the domain name for your server: " domain
 read -p "Enter the email address for LetsEncrypt: " email
 
 echo -n "Enter the password for the WG Admin page: "
 read -s pw
 
-wget "https://raw.githubusercontent.com/runberg/easy-wg-vps/main/docker-compose.yml"
+until [ -f docker-compose.yml ]
+do
+     echo "Docker file still downloading. Waiting ..."
+     sleep 5
+done
 
-echo "sed -i -e 's/<DOMAIN>/$domain/g' /docker-compose.yml"
+echo "sed -i -e 's/<DOMAIN>/$domain/g' docker-compose.yml"
 sed -i -e 's/<DOMAIN>/$domain/g' docker-compose.yml
-echo "sed -i -e 's/<PASSWORD>/$pw/g' /docker-compose.yml"
+echo "sed -i -e 's/<PASSWORD>/$pw/g' docker-compose.yml"
 sed -i -e 's/<PASSWORD>/$pw/g' docker-compose.yml
-echo "sed -i -e 's/<EMAIL>/$email/g' /docker-compose.yml"
+echo "sed -i -e 's/<EMAIL>/$email/g' docker-compose.yml"
 sed -i -e 's/<EMAIL>/$email/g' docker-compose.yml
 
 # -----------------------------------
